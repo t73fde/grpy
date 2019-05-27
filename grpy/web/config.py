0a62d96f3833eb73742d389eb1605dd589665fbf
Detlef Stern <detlef.stern@hs-heilbronn.de>
2019-05-27 12:40:14 +0200

    Add some information for configuration parameters

--- grpy/web/config.py
+++ grpy/web/config.py
@@ -21,12 +21,32 @@
 
 import platform
 
+# Secret key for encryption of user sessions and some other data.
+# This default key should be sufficiently safe, as long the host name is not
+# known to others. If you run Grpy inside a Docker container, this is mostly
+# true. For additional security, please provide a sufficient long random key.
+# More information: http://flask.pocoo.org/docs/1.0/config/#SECRET_KEY
 SECRET_KEY = "".join(tuple(platform.uname()._asdict().values()) +
                      platform.python_build())
 
+# Specification of the repository that stores the data. Possible values:
+# * "ram://": a RAM-based repository, not useful for production; fallback
+#             if everything fails.
+# * "sqlite:///": a SQLite-based repository, that stores the data in memory
+#             too. Not useful for production, but for debugging.
+# * "sqlite:PATH": a SQLite-based repository, that stores data in a file. PATH
+#             is relative to current directory, e.g. "sqlite:db/grpy.sqlite3".
+# * "sqlite://PATH": a SQLite-based repository. PATH is an absolute name, must
+#             start with a "/", e.g. "sqlite:///var/db/grpy.sqlite3".
 REPOSITORY = "ram://"
 
+# Level of logging. See: https://docs.python.org/3/library/logging.html#logging-levels
 LOG_LEVEL = "NOTSET"
 
+# Endpoint that provides Basic Authentication
+AUTH_URL = "http://localhost:9876/"
+
+# Specifies whether user names are case sensitive or not, e.g. whether "USER"
+# and "user" are different user identifications or not.
 # Per default, user names are not case sensitive
 AUTH_CASE = False
